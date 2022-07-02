30d6e680a7a8f1db76fc73293fd7d91f65ac9ea0
Nishit Pandit <nishitpandit@ymail.com>
2022-07-02 18:45:09 +0530

    separate file for okta login

---
+++ routes/okta.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+const userModel = require('../models/user')
+
+
+router.post("/", async(req, res) => {
+    try {
+        await userModel.findOneAndUpdate(
+            {userName : req.cookie['username']},
+            {
+                oktaDomain: req.body.oktaDomain,
+                oktaAPIKey: req.body.oktaAPIKey
+            },
+            {new: true }
+        )
+        res.sendStatus(200)
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+})
+
+module.exports = router
