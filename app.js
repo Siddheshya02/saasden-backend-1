a647f845b335a27be1b59879a5b2e9de93cd3624
Nishit Pandit <nishitpandit@ymail.com>
2022-07-04 14:42:12 +0530

    cors change

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'))
 
 var corsOptions = {
-    origin: 'http://localhost:3001',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
   }
 
 
