da0c39dc28bde0e00cc76e02c71f60f2acf3d1da
Nishit Pandit <nishitpandit@ymail.com>
2022-07-04 14:10:08 +0530

    made changes to port

--- app.js
+++ app.js
@@ -74,8 +74,7 @@ app.get("/cookies",(req, res)=>{
     res.send(req.cookies)
 })
 
-app.listen(3000,()=>{
-    console.log('Listening to port 3000')
-})
+const port=process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 
