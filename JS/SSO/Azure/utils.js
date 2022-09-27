ac4804e6d504ef0cf202c3e5abd5d3edf8ac83cd
Nishit Pandit <nishitpandit@ymail.com>
2022-09-27 21:42:16 +0530

    merged azure routes and functions

---
+++ JS/SSO/Azure/utils.js
@@ -0,0 +1,134 @@
+const axios = require('axios')
+const subModel = require('../../../models/subscription')
+const empModel = require('../../../models/employee')
+const domain = 'https://graph.microsoft.com/.default'
+// get onelogin access token
+async function getToken (clientID, clientSecret, tenantId) {
+  const token = await axios.post(
+        `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`,
+        new URLSearchParams({
+          client_id: `${clientID}`,
+          scope: `${domain}`,
+          client_secret: `${clientSecret}`,
+          grant_type: 'client_credentials'
+        })
+  ).then(res => { return res.data.access_token }).catch(res => console.log(res))
+  return token
+}
+
+// get list of apps used by the org
+async function getApps (accessToken) {
+  const apps = await axios.get('https://graph.microsoft.com/v1.0/applications', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  }).then(res => { return res.data.value }).catch(res => console.log(res))
+  const len_apps = apps.length
+  const finalAppDetails = []
+  for (let i = 0; i < len_apps; i++) {
+    const { appId, displayName } = apps[i]
+    const users = []
+    const len_user = apps[i].appRoles.length
+    for (let j = 0; j < len_user; j++) {
+      const { id, displayName } = apps[i].appRoles[j]
+      const userObject = { userID: id, userName: displayName }
+      users.push(userObject)
+    }
+    const appObject = { appID: appId, appName: displayName, users: users }
+    finalAppDetails.push(appObject)
+  }
+  for (const app of finalAppDetails) {
+    app.appName = app.appName.toLowerCase()
+  }
+  return finalAppDetails
+}
+
+// get list of users in the org
+async function getUsers (accessToken) {
+  const finalUserDetails = []
+  const list = await axios.get('https://graph.microsoft.com/v1.0/users/', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  }).then(res => { return res.data.value }).catch(res => console.log(res))
+  for (let i = 0; i < list.length; i++) {
+    const { id, displayName } = list[i]
+    const dat = await axios.get(`https://graph.microsoft.com/v1.0/users/${list[i].id}/appRoleAssignments`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    }).then(res => { return res.data.value }).catch(res => console.log(res))
+    const apps = []
+    for (let j = 0; j < dat.length; j++) {
+      const { id, resourceDisplayName } = dat[j]
+      const appsObject = { appID: id, appName: resourceDisplayName }
+      apps.push(appsObject)
+    }
+    const userObject = { userID: id, userName: displayName, apps: apps }
+    finalUserDetails.push(userObject)
+  }
+  return finalUserDetails
+}
+
+async function getSubs (accessToken, saasdenID) {
+  const subList = []
+  const appList = await getApps(accessToken)
+
+  for (const app of appList) {
+    const emps = []
+    for (const user of app.users) {
+      emps.push({
+        id: user.userID,
+        username: user.userName
+      })
+    }
+
+    subList.push({
+      ssoID: app.appID,
+      name: app.appName,
+      emps: emps
+      // data to be fetched from EMS
+      // emsID: String,
+      // licences: Number,
+      // currentCost: Number,
+      // amountSaved: Number,
+      // dueData: String
+    })
+  }
+
+  const filter = { saasdenID: saasdenID }
+  const update = { apps: subList }
+  await subModel.findOneAndUpdate(filter, update)
+  console.log('Azure subscription data updated successfully')
+}
+
+async function getEmps (accessToken, saasdenID) {
+  const userList = []
+  const empList = await getUsers(accessToken)
+
+  for (const emp of empList) {
+    const appList = emp.apps
+    const userAppList = []
+    for (const app of appList) {
+      userAppList.push({
+        name: app.appName.toLowerCase(),
+        id: app.appID
+      })
+    }
+    userList.push({
+      id: emp.userId,
+      //   email: emp.email,
+      //   firstname: emp.firstname,
+      userName: emp.userName,
+      //   lastname: emp.lastname,
+      apps: userAppList
+    })
+  }
+
+  const filter = { saasdenID: saasdenID }
+  const update = { emps: userList }
+  await empModel.findOneAndUpdate(filter, update)
+  console.log('Azure employee data updated successfully')
+}
+
+module.exports = { getToken, getSubs, getEmps }
--- routes/SSO/Azure_route.js
+++ routes/SSO/Azure_route.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
-const ssoSchema = require('../../models/sso')
+const ssoModel = require('../../models/sso')
+const utils = require('../../JS/SSO/Azure/utils')
 const subSchema = require('../../models/subscription')
 const empSchema = require('../../models/employee')
 
-router.get('/auth', async (req, res) => {
+router.post('/auth', async (req, res) => {
   // need to add a cookie with _id from user schema
-  const clientID = req.cookies.user_saasden_id
-  const filter = { clientID }
+  // const clientID = req.cookies.user_saasden_id
+  const filter = { saasdenID: req.cookies.saasdenID }
   const update = {
-    envID: req.body.envID,
-    apiToken: req.body.apiToken
+    clientID: req.body.clientID,
+    clientSecret: req.body.clientSecret,
+    tenantId: req.body.tenantId
   }
+  console.log('client  ', update)
   try {
-    await ssoSchema.findOneAndDelete(filter, update)
-    console.log('Azure Credentials saved succesfully')
+    const user = await ssoModel.findOneAndUpdate(filter, update)
+    console.log('Azure credentials saved successfully  =>', user)
     res.sendStatus(200)
   } catch (error) {
     console.log(error)
@@ -26,6 +29,10 @@ router.get('/auth', async (req, res) => {
 router.get('/refreshData', async (req, res) => {
   try {
     // fetch SSO Data from the DB
+    const ssoData = await ssoModel.findOne({ saasdenID: req.cookies.saasdenID })
+    const accessToken = await utils.getToken(ssoData.clientID, ssoData.clientSecret, ssoData.tenantId)
+    await utils.getSubs(accessToken, ssoData.saasdenID)
+    await utils.getEmps(accessToken, ssoData.saasdenID)
     res.sendStatus(200)
   } catch (error) {
     console.log(error)
