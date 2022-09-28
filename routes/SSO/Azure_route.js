7a9a3c9da2eeea1e4c6cdb4cd9fc2467a5ae5400
Nishit Pandit <nishitpandit@ymail.com>
2022-09-28 23:35:46 +0530

    organized to azure route in acc with the current layout

--- routes/SSO/Azure_route.js
+++ routes/SSO/Azure_route.js
@@ -1,24 +1,47 @@
-const express = require('express')
+import axios from 'axios'
+import express from 'express'
+import orgSchema from '../../models/organization.js'
 const router = express.Router()
 
-const ssoModel = require('../../models/sso')
-const utils = require('../../JS/SSO/Azure/utils')
-const subSchema = require('../../models/subscription')
-const empSchema = require('../../models/employee')
+router.get('/', async (req, res) => {
+  try {
+    const orgData = await orgSchema.find({ name: req.session.orgName })
+    req.session.sso_tenantID = orgData.ssoData.tenantID
+    req.session.sso_clientID = orgData.ssoData.clientID
+    req.session.sso_clientSecret = orgData.ssoData.clientSecret
+
+    const tokenSet = await axios.post(`https://${req.session.sso_apiDomain}/oauth2/v2.0/token`,
+      new URLSearchParams({
+        client_id: `${req.session.sso_clientID}`,
+        scope: `${req.session.sso_apiDomain}`,
+        client_secret: `${req.session.sso_clientSecret}`,
+        grant_type: 'client_credentials'
+      })
+    )
+
+    req.session.sso_accessToken = tokenSet.access_token // access token
+    // req.session.sso_refreshToken = tokenSet.refresh_token // refresh token
+
+    res.sendStatus(200)
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
 
 router.post('/auth', async (req, res) => {
-  // need to add a cookie with _id from user schema
-  // const clientID = req.cookies.user_saasden_id
-  const filter = { saasdenID: req.cookies.saasdenID }
+  const filter = { name: req.session.orgName }
   const update = {
-    clientID: req.body.clientID,
-    clientSecret: req.body.clientSecret,
-    tenantId: req.body.tenantId
+    ssoData: {
+      clientID: req.body.clientID,
+      clientSecret: req.body.clientSecret,
+      tenantID: req.body.tenantID
+    }
   }
-  console.log('client  ', update)
+
   try {
-    const user = await ssoModel.findOneAndUpdate(filter, update)
-    console.log('Azure credentials saved successfully  =>', user)
+    await orgSchema.findOneAndUpdate(filter, update)
+    console.log('Azure credentials saved successfully')
     res.sendStatus(200)
   } catch (error) {
     console.log(error)
@@ -26,18 +49,23 @@ router.post('/auth', async (req, res) => {
   }
 })
 
-router.get('/refreshData', async (req, res) => {
-  try {
-    // fetch SSO Data from the DB
-    const ssoData = await ssoModel.findOne({ saasdenID: req.cookies.saasdenID })
-    const accessToken = await utils.getToken(ssoData.clientID, ssoData.clientSecret, ssoData.tenantId)
-    await utils.getSubs(accessToken, ssoData.saasdenID)
-    await utils.getEmps(accessToken, ssoData.saasdenID)
-    res.sendStatus(200)
-  } catch (error) {
-    console.log(error)
-    res.sendStatus(500)
-  }
-})
+// access_token => req.sesion.sso_accessToken
+// client ID => req.session.sso_clientID
+// client Secret => req.session.sso_clientSecret
+// tenant ID => req.session.sso_tenantID
 
-module.exports = router
+// router.get('/refreshData', async (req, res) => {
+//   try {
+//     // fetch SSO Data from the DB
+//     const ssoData = await ssoModel.findOne({ saasdenID: req.cookies.saasdenID })
+//     const accessToken = await utils.getToken(ssoData.clientID, ssoData.clientSecret, ssoData.tenantId)
+//     await utils.getSubs(accessToken, ssoData.saasdenID)
+//     await utils.getEmps(accessToken, ssoData.saasdenID)
+//     res.sendStatus(200)
+//   } catch (error) {
+//     console.log(error)
+//     res.sendStatus(500)
+//   }
+// })
+
+export { router }
