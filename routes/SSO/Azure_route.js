e2bbd39062713c070d7bdc53b13e9a1185ac83d8
bhattji007 <shubhambhattrocks123@gmail.com>
2022-10-02 19:12:22 +0530

    testing

--- routes/SSO/Azure_route.js
+++ routes/SSO/Azure_route.js
@@ -81,11 +81,11 @@ router.post('/auth', async (req, res) => {
 router.get('/refreshData', async (req, res) => {
   console.log('Fetching Azure Data')
   const ems_creds = {
-    name: 'xero',
-    domain: null,
-    tenantID: '61608321-d0a3-408f-9e5a-98cba0de0fee',
-    accessToken: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjFDQUY4RTY2NzcyRDZEQzAyOEQ2NzI2RkQwMjYxNTgxNTcwRUZDMTkiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJISy1PWm5jdGJjQW8xbkp2MENZVmdWY09fQmsifQ.eyJuYmYiOjE2NjQ3MTE5MjIsImV4cCI6MTY2NDcxMzcyMiwiaXNzIjoiaHR0cHM6Ly9pZGVudGl0eS54ZXJvLmNvbSIsImF1ZCI6Imh0dHBzOi8vaWRlbnRpdHkueGVyby5jb20vcmVzb3VyY2VzIiwiY2xpZW50X2lkIjoiQ0ExMkMyMjU2MTI0NDI1NkE1NzIxRjgyQ0JBODBFNTIiLCJzdWIiOiI4OTIyNDVkNzBiNDM1ZGEzOTE2YTI3MDQwMjczOTRhOCIsImF1dGhfdGltZSI6MTY2NDY4OTc2OSwieGVyb191c2VyaWQiOiJkOGEwZTY2Zi0xNmYxLTRhMDctYmY1Yi01NTE4OTBiNGJlM2MiLCJnbG9iYWxfc2Vzc2lvbl9pZCI6IjdiMDYwNTUyZTJjMjQwZmNhZDA3YjllZmY4ZGQzNzM4IiwianRpIjoiNTJDNjA3MjBGMUVDNDI1RDFFMTYzNTg5Mjk0MUU4MDMiLCJhdXRoZW50aWNhdGlvbl9ldmVudF9pZCI6ImYyMzNkNjNlLTZhMzgtNGIwZC05NmE0LTIzNjliZjIzN2I4ZiIsInNjb3BlIjpbImVtYWlsIiwicHJvZmlsZSIsIm9wZW5pZCIsImFjY291bnRpbmcuc2V0dGluZ3MiLCJhY2NvdW50aW5nLnRyYW5zYWN0aW9ucyIsImFjY291bnRpbmcudHJhbnNhY3Rpb25zLnJlYWQiLCJhY2NvdW50aW5nLmNvbnRhY3RzIiwib2ZmbGluZV9hY2Nlc3MiXSwiYW1yIjpbInB3ZCJdfQ.gx8FvO2jk_p6VHtnLI39X-lNxvf4Nnzep7KHk6NW67cqinU3YL82xARiFr9KBdvb-XkctObU_Fm6WLjyZp5hcmezqFtzxeLyed2rj0nq3PFqlqWGuVR0y0BiOT3E0rTSMd1yVmrXUGFh-p3SfEuc1GVmnc-xQ2glQIdUHy-y8AYbwz-4sV4WBFsFkxQSaQVcjOacxrQ9PxpMfZLPfb5RTzeQ61VAMnsvcJ8qxVinULmbF7y3Vaq9uSVgtESLRqlS4gnjCj2hNApBA8D03kBQNLuKbiLW8rXRysfffKwCgBarqy8MYXduUGG2hPOBgBsjYmd2z_qZ0IzPkkudcUjZYg',
-    apiToken: null
+    name: req.session.ems_name,
+    domain: undefined,
+    tenantID: undefined,
+    accessToken: req.session.ems_accessToken,
+    apiToken: req.session.ems_IDToken
   }
   const sso_creds = {
     name: undefined,
