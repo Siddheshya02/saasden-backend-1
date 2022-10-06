d993d99badbb5543ff5447b3467c569dd599426c
Nishit Pandit <nishitpandit@ymail.com>
2022-10-06 21:49:11 +0530

    cors fix

--- app.js
+++ app.js
@@ -55,8 +55,9 @@ const sess_config = {
 }
 
 const cors_config = {
-  origin: ['*'],
-  methods: ['GET', 'POST', 'DELETE']
+  origin: ['https://login.xero.com', 'https://accounts.zoho.in', 'http://localhost:3000', 'http://127.0.0.1:3000'],
+  methods: ['GET', 'POST', 'DELETE'],
+  credentials: true
 }
 
 // Redis server connection
