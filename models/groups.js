cc3f4ba3503226cda071a195e0a1a3f94782312e
VadirajKranam <vadirajraokaranam@gmail.com>
2023-01-11 17:12:31 +0530

    groups added

---
+++ models/groups.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const groupModel = new Schema({
+  ID: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  groups: [{
+    name: String,
+    groupId: String,
+    emps: [{
+      id: String,
+      email: String,
+      firstname: String,
+      username: String,
+      lastname: String
+    }],
+    apps: [{
+      id: String,
+      name: String
+    }]
+  }]
+})
+const groupSchema = mongoose.model('groupSchema', groupModel)
+export default groupSchema
