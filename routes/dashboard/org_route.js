11f4ae4d899c770e0cad0d99595f74ddc15d87e2
VadirajKranam <vadirajraokaranam@gmail.com>
2023-02-02 23:33:37 +0530

    org check updated 2.o

--- routes/dashboard/org_route.js
+++ routes/dashboard/org_route.js
@@ -4,8 +4,12 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
   try {
-    const orgData = await orgSchema.find({ ID: req.session.orgID })
-    res.json(orgData)
+    const orgData = await orgSchema.findOne({ ID: req.session.orgID })
+    const ssoNames = []
+    for (let i = 0; i < orgData.ssoData.length; i++) {
+      ssoNames.push(orgData.ssoData[i].ssoName)
+    }
+    res.json(ssoNames)
   } catch (error) {
     console.log(error)
     res.sendStatus(500)
