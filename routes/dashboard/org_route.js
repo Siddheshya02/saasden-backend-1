8d6425c0cafaa7ad265210827367776264858b49
VadirajKranam <vadirajraokaranam@gmail.com>
2023-02-02 22:23:46 +0530

    org check added

--- routes/dashboard/org_route.js
+++ routes/dashboard/org_route.js
@@ -5,7 +5,11 @@ const router = express.Router()
 router.get('/', async (req, res) => {
   try {
     const orgData = await orgSchema.find({ ID: req.session.orgID })
-    res.json(orgData)
+    if (orgData.ssoData != null && orgData.ssoData.length > 0) {
+      res.sendStatus(200)
+    } else {
+      res.sendStatus(201)
+    }
   } catch (error) {
     console.log(error)
     res.sendStatus(500)
