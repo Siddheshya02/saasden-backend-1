6a54f1a955f210cf9a3c542980e80a706e35c9b5
Shubham Bhatt <shubham@saasden.club>
2023-02-03 19:39:07 +0530

    Update middleware.js

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,6 +4,7 @@ import jwt_decode from 'jwt-decode'
 export function setOrgName (req, res, next) {
   const auth0_tokenSet = jwt_decode(req.get('Authorization'))
   req.session.orgID = auth0_tokenSet.org_id
+  console.log('orgname setup')
   next()
 }
 export function setSSOs (req, res, next) {
