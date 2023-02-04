101230b2756a15bac6b4a2e43ea8764cf80e664f
Shubham Bhatt <shubham@saasden.club>
2023-02-04 19:35:16 +0530

    testing

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,8 +4,8 @@ import jwt_decode from 'jwt-decode'
 export function setOrgName (req, res, next) {
   const auth0_tokenSet = jwt_decode(req.get('Authorization'))
   req.session.orgID = auth0_tokenSet.org_id
-  // console.log(req.session)
-  // console.log('orgname setup')
+  console.log(req.session)
+  console.log('orgname setup')
   next()
 }
 export function setSSOs (req, res, next) {
@@ -13,7 +13,7 @@ export function setSSOs (req, res, next) {
   console.log(req.session)
   if (!req.session.ssos) {
     req.session.ssos = []
-    // console.log('session reset')
+    console.log('session reset')
     next()
   } else {
     // console.log(req.session)
