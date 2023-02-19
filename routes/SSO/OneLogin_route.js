706db9524fa053340dca32dde4c2a2eedc242d6a
VadirajKranam <vadirajraokaranam@gmail.com>
2023-02-19 19:39:28 +0530

    checking for session error

--- routes/SSO/OneLogin_route.js
+++ routes/SSO/OneLogin_route.js
@@ -114,7 +114,7 @@ router.get('/', async (req, res) => {
               break
             }
           }
-          // console.log(req.session.ssos)
+          console.log(req.session.ssos)
           break
         }
       }
