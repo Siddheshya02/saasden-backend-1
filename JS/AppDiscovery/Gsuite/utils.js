f4ff4d63a1f175ffd50644e69e97e2cf91289ebb
VadirajKranam <vadirajraokaranam@gmail.com>
2023-03-07 19:27:45 +0530

    gsuite customer id field added in session sso data

--- JS/AppDiscovery/Gsuite/utils.js
+++ JS/AppDiscovery/Gsuite/utils.js
@@ -43,14 +43,11 @@ export async function getGsuiteToken (code, client_id, client_secret) {
 }
 export async function getApps (access_token, customerId) {
   const curDate = new Date().toISOString().split('T')[0]
-  const apps = axios.get(`https://admin.googleapis.com/admin/reports/v1/usage/dates/2022-12-21?customerId=${customerId}}`, {
+  const apps = await axios.get(`https://admin.googleapis.com/admin/reports/v1/usage/dates/2022-12-21?customerId=${customerId}`, {
     headers: {
       Authorization: `Bearer ${access_token}`,
       Accept: 'application/json'
     }
-  }).then(function (response) {
-    console.log(JSON.stringify(response.data))
-    // console.log('User created sucessfully')
   })
     .catch(function (error) {
       console.log(error.response.data.error)
