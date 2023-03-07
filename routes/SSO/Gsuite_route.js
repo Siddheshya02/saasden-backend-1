0a385dcf08c0db67accecea1264e9bdd649f2b9b
VadirajKranam <vadirajraokaranam@gmail.com>
2023-03-07 20:12:15 +0530

    gsuite customer id field added in session sso data

--- routes/SSO/Gsuite_route.js
+++ routes/SSO/Gsuite_route.js
@@ -122,7 +122,7 @@ router.get('/callback', async (req, res) => {
 })
 
 router.post('/createUser', async (req, res) => {
-  const user = req.body
+  const user = req.body.userInfo
   // console.log('user ', user)
   try {
     for (const sso of req.session.ssos) {
