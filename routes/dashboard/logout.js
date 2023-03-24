3da0134ca09b99a193d05806b09ecf3801b821a4
goldsnort <dheerajrajput7056@gmail.com>
2023-03-24 17:47:45 +0000

    logout fixed

--- routes/dashboard/logout.js
+++ routes/dashboard/logout.js
@@ -4,6 +4,6 @@ const router = express.Router()
 router.post('/', (req, res) => {
   req.session.destroy()
   console.log('User logged out')
-  res.send(200)
+  res.sendStatus(200)
 })
 export { router }
